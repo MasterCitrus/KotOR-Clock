84a68b975402025b92223fcf6bab97010824445f
MasterCitrus <45452554+MasterCitrus@users.noreply.github.com>
2024-12-31 21:29:40 +1100

    More utility functions

--- common/utils.js
+++ common/utils.js
@@ -5,3 +5,44 @@ export function zeroPad(i) {
     if(i < 10) i = "0" + i;
     return i;
 }
+
+//Gets first 3 letters of day from index
+export function getDay3(i) {
+    switch(i){
+        case 0:
+            return "SUN";
+        case 1:
+            return "MON";
+        case 2:
+            return "TUE";
+        case 3:
+            return "WED";
+        case 4:
+            return "THU";
+        case 5:
+            return "FRI";
+        case 6:
+            return "SAT";
+    }
+}
+
+//Gets day from index
+export function getDay(i) {
+    switch(i) {
+        case 0:
+            return "Sunday";
+        case 1:
+            return "Monday";
+        case 2:
+            return "Tuesday";
+        case 3:
+            return "Wednesday";
+        case 4:
+            return "Thursday";
+        case 5:
+            return "Friday";
+        case 6:
+            return "Saturday";
+    }
+}
+
